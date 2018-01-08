9b84d44aa0e007fbbbea8b8d43de48e57a7cf758
albertchan <albert.shun.dat.chan@gmail.com>
2018-01-07 22:12:33 -0500

    Fix Func class

--- src/main/java/com/javalbert/biexprtree/Func.java
+++ src/main/java/com/javalbert/biexprtree/Func.java
@@ -1,9 +1,6 @@
 package com.javalbert.biexprtree;
 
 public interface Func {
-	default FunctionType getFunctionType() {
-		return getOperatorInfo().getFunctionType();
-	}
 	default String getOperator() {
 		return getOperatorInfo().getOperator();
 	}
