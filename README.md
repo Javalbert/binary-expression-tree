4924153eaf7f33c7e0b3d52fc0712cb4ab544266
albertchan <albert.shun.dat.chan@gmail.com>
2018-01-27 09:06:20 -0500

    Test parsing expressions from ExpressionPrinter

--- README.md
+++ README.md
@@ -5,10 +5,10 @@ A [binary expression tree](https://en.wikipedia.org/wiki/Binary_expression_tree)
 ## Design & Features
 
 - Infix notation
-- Supported numeric types: int, long, float, double, BigInteger, BigDecimal
-- Basic math operators: +, -, /, *, %, ** (exponentiation)
-- Basic boolean operators: =, !=, <, <=, >, >=
-- Unary operators: -, !
+- Supported types: `String, int, long, float, double, BigInteger, BigDecimal`
+- Basic math operators: `+, -, /, *, %, ** (exponentiation)`
+- Basic boolean operators: `=, !=, <, <=, >, >=`
+- Unary operators: `-, !`
 - Expressions are built by chaining method calls instead of passing a string
 - Nested (i.e. parenthesized) expressions
 - Variables are mutable objects that can change the result of the next evaluation
@@ -17,3 +17,4 @@ A [binary expression tree](https://en.wikipedia.org/wiki/Binary_expression_tree)
 - Left-to-right evaluation
 - Short-circuit evaluation
 - Expression parser uses the same Expression API to build an expression
+- Expression printer outputs strings that can be parsed back
--- src/test/groovy/com/javalbert/biexprtree/ExpressionPrinterSpec.groovy
+++ src/test/groovy/com/javalbert/biexprtree/ExpressionPrinterSpec.groovy
@@ -110,4 +110,34 @@ class ExpressionPrinterSpec extends Specification {
 		then: '"Albert \\"Javalbert" Chan\\" is returned'
 		str == '"Albert \\"Javalbert\\" Chan"'
 	}
+	
+	def 'Expression string with unary operator and nested expression can be parsed back'() {
+		given: '-1 * (2 + 3)'
+		String exprString = '-1 * (2 + 3)'
+		
+		when: 'parsed'
+		Expression expr = new ExpressionParser().parse(exprString)
+		
+		and: 'parsed Expression is printed'
+		String exprString2 = new ExpressionPrinter().print(expr)
+		
+		then: 'the resulting expression string is equal to the original expression string: -1 * (2 + 3)'
+		exprString == exprString2
+	}
+	
+	def 'Expression string with a variable and quotes within string value can be parsed back'() {
+		given: 'name = "Albert \\"Javalbert\\" Chan"'
+		String exprString = 'name = "Albert \\"Javalbert\\" Chan"'
+		
+		when: 'parsed'
+		ExpressionParser parser = new ExpressionParser()
+		parser.addVariable(new RefVariable(String, 'name'))
+		Expression expr = parser.parse(exprString)
+		
+		and: 'parsed Expression is printed'
+		String exprString2 = new ExpressionPrinter().print(expr)
+		
+		then: 'the resulting expression string is equal to the original expression string: name = "Albert \\"Javalbert\\" Chan"'
+		exprString == exprString2
+	}
 }
